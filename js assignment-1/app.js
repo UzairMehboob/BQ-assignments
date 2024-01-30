6bcbf82148eeae3d11f4145d53b558f9389d7515
UzairMehboob <uziuzair332@gmail.com>
2024-01-30 23:11:38 +0500

    js assignment

---
+++ js assignment-1/app.js
@@ -0,0 +1,78 @@
+// Question-1
+alert("Welcome to Our Website");
+document.write("Question 1")
+
+// Question-1
+alert("<p>Error! Please Enter a valid password</p>");
+
+
+// Question-1
+alert(" uzair");
+
+// Question-1
+alert("Alert 1!");
+alert("Alert 2!");
+alert("Alert 3!");
+
+
+// Question-7
+var username;
+
+// Question-8
+var myName  = "uzair mehboob";
+
+// Question-9
+var message="Hello World";
+alert(message);
+
+// Question-10
+var stdName = 'bahadur';
+var stdAge = 19;
+var stdDept= 'bio';
+var stdGrade='c';
+alert('Student Name is ' +stdName );
+alert('Student Age is ' +stdAge );
+alert('Department is ' +stdDept );
+alert('Student Grade is' +stdGrade);
+
+// Question-11
+var email ="uziuzair332@gmail.com" ;
+
+// Question-12
+alert("My email address is: ");
+
+// Question-3
+var str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(str);
+
+// Question-14
+var age = 18;
+alert("I am " +age + " years old");
+
+// Question-15
+var visitCount = 0;
+visitCount++;
+alert("You have visited this site " + visitCount + " times");
+alert("You have visited this site " + visitCount + " times");
+
+
+// Question-16
+var birthYear = 1998
+alert(birthYear)
+document.write("I was born in: "  + birthYear )
+
+// Question-17
+
+var visitorName = prompt("Enter your name here: ");
+var productTitle = prompt("What do you want to buy? ");
+var quantity = prompt("How much do you want to buy?");
+
+document.write("<h1>"+visitorName+" ordered "+quantity+" "+productTitle+"(s) on baloch Clothing Store</h1>")
+
+
+
+// Question-18
+var num1=+prompt("Please enter first number: ")
+var num2=+prompt("Please enter second number: ")
+var add =  (num1+num2);
+alert("The sum of addition is: " + add );
